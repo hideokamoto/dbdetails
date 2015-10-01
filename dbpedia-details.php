d48e200d4ca35c4ea9f09baefd89715c984e6ddc
Hidetaka Okamoto <hide.okamoto@digitalcube.jp>
2015-10-02 00:47:56 +0900

    initial commit

---
+++ dbpedia-details.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Dbpedia-details
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: dbpedia-details
+Domain Path: /languages
+*/
+
