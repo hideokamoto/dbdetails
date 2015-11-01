7cf2ddaa19fa95781f337c587a3e64a1d3d78fd7
Hidetaka Okamoto <hide.okamoto@digitalcube.jp>
2015-11-01 14:37:17 +0900

    Update bata1

--- dbpedia-details.php
+++ dbpedia-details.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Dbpedia-details
 Version: 0.1-alpha
-Description: PLUGIN DESCRIPTION HERE
+Description: DBPedia日本語版から説明文を取得してポップアップ表示させるプラグインです。
 Author: Hideokamoto
 Author URI: http://wp-kyoto.net/
 Plugin URI: PLUGIN SITE HERE
@@ -10,32 +10,29 @@ Text Domain: dbpedia-details
 Domain Path: /languages
 */
 
-add_shortcode ( 'detail' , 'dbdetails_details');
-add_action('wp_footer', 'dbdetails_script');
+add_shortcode ( 'db-detail' , 'dbdetails_details');
 
 function dbdetails_details( $atts, $content = '' ){
   if ( !$content )
     return;
 
-  return "<div data-dbdetails='{$content}' class='dbdetails'>{$content}</div>";
+  $quote = dbdetails_get_quote($content);
+  return "<span data-dbdetails='{$content}' class='dbdetails'>{$content}{$quote}</span>";
 }
 
-function dbdetails_script() {
-  $script  = "<script>";
-  $script .= "(function($) {";
-  $script .= "var keywords = encodeURIComponent($('.dbdetails').data('dbdetails'));";
-  $script .= "console.log(keywords);";
-  $script .= "$.ajax({";
-  $script .= "  url: 'http://ja.dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fja.dbpedia.org&query=select+distinct+*+where+%7B+%3Chttp%3A%2F%2Fja.dbpedia.org%2Fresource%2F' + keywords + '%3E+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2Fabstract%3E+%3Fo+.+%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on',";
-  $script .= "  type:'GET',";
-  $script .= "  dataType: 'json',";
-  $script .= "  timeout:10000,";
-  $script .= "}).done(function(data) {";
-  $script .= "  var html = '<blockquote>' + data.results.bindings[0].o.value + '</blockquote>';";
-  $script .= "  $('.dbdetails').append(html);";
-  $script .= "}).fail(function(data) {";
-  $script .= "  var html = '<blockquote>failed</blockquote>';";
-  $script .= "  $('.dbdetails').append(html);";
-  $script .= "});})(jQuery);</script>';";
-  echo $script;
+function dbdetails_get_quote($content){
+
+  $keyword = urlencode($content);
+  $url = "http://ja.dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fja.dbpedia.org&query=select+distinct+*+where+%7B+%3Chttp%3A%2F%2Fja.dbpedia.org%2Fresource%2F{$keyword}%3E+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2Fabstract%3E+%3Fo+.+%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on";
+  $quote = wp_remote_get($url);
+  $quote = json_decode($quote["body"])->results->bindings[0]->o->value;
+  $html = "<span class='dbdetails-quote'>{$quote}</span>";
+  return $html;
 }
+
+function dbdetail_scripts() {
+	wp_enqueue_style( 'dbdetail-style', plugin_dir_url( __FILE__ ) . 'inc/dbdetail-style.css' );
+	wp_enqueue_script( 'dbdetail-script', plugin_dir_url( __FILE__ ) . '/inc/dbdetail.js', array(), '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'dbdetail_scripts' );
---
+++ inc/dbdetail.js
@@ -0,0 +1,5 @@
+(function($) {
+  $(".dbdetails").click(function () {
+    $(this).toggleClass("active");
+  });
+})(jQuery);
